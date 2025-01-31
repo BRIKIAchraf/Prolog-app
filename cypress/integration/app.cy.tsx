9392491dfad92a3e3226d2d33a4d656e2fb24e98
ashraf breki <brikiachraf@DJERBA.r-iset.tn>
2025-01-31 23:51:13 +0100

    Delete the cypress mock test

--- cypress/integration/app.cy.tsx
+++
@@ -1,9 +0,0 @@
-describe("Home Page", () => {
-  it("should have an h1 element", () => {
-    // Visit the homepage of your Next.js app
-    cy.visit("http://localhost:3000");
-
-    // Check that the h1 element is found
-    cy.get("h1").should("exist");
-  });
-});
